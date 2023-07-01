24623230e526174a5f00605f153be821d493ccd8
Ravil Gasanov <12788769+ravil-gasanov@users.noreply.github.com>
2023-07-01 16:24:37 +0400

    add data + train_test split

--- src/data/train_test_split.py
+++ src/data/train_test_split.py
@@ -12,24 +12,18 @@ np.random.seed(SEED)
 
 def split(raw_path, train_path, test_path):
     df = pd.read_csv(raw_path)
-
-    train, test = train_test_split(df)
+    
+    train = df[df.JAHR <= 2020]
+    test = df[df.JAHR > 2020]
 
     train.to_csv(train_path, index = False)
     test.to_csv(test_path, index = False)
 
 
 if __name__ == "__main__":
-    raw_path = "./data/raw/data.csv"
+    raw_path = "./data/raw/monatszahlen2209_verkehrsunfaelle.csv"
     train_path = "./data/train_test/train.csv"
     test_path = "./data/train_test/test.csv"
-
-    parser = argparse.ArgumentParser(description='Description of your program.')
-    parser.add_argument('-rn', '--raw_path', help='Path to the raw data file.', required=False)
-    args = parser.parse_args()
-    
-    if args.raw_path:
-        raw_path = args.raw_path
     
     split(raw_path, train_path, test_path)
 
