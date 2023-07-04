b405202a45cb038c0d4e65761dad8922884c3ef7
Ravil Gasanov <12788769+ravil-gasanov@users.noreply.github.com>
2023-07-04 09:43:04 +0400

    added unit tests for preprocessing funcs

--- src/tests/test_preprocessing.py
+++ src/tests/test_preprocessing.py
@@ -3,10 +3,10 @@ import pandas as pd
 
 from ..preprocessing import *
 
-def test_extract_month():
+def test_make_month():
     df = pd.DataFrame({'month': ['201001', '201002', '201003'], 'correct': ['01', '02', '03']})
     
-    df['month'] = extract_month(df['month'])
+    df['month'] = make_month(df['month'])
 
     assert (df['month'] == df['correct']).all()
 
@@ -22,3 +22,14 @@ def test_drop_yearly_aggregate_rows():
     df = drop_yearly_aggregate_rows(df)
 
     assert (df['month'] == correct['month']).all()
+
+def test_make_date():
+    df = pd.DataFrame({
+        'month': ['01', '02'], \
+        'year': [2010, 2010],
+        'correct': ['2010-01', '2010-02']
+        })
+    
+    df['date'] = make_date(df['year'], df['month'])
+
+    assert (df['date'] == df['correct']).all()
