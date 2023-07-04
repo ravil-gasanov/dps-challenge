d491cb59859d429681a67ccab70231b4be027d0c
Ravil Gasanov <12788769+ravil-gasanov@users.noreply.github.com>
2023-07-04 09:42:22 +0400

    added make_date + renamed extract_month to make_month

--- src/preprocessing/make_features.py
+++ src/preprocessing/make_features.py
@@ -1,8 +1,16 @@
 import pandas as pd
 
-def extract_month(month):
+def make_month(month):
     '''
     month: pd.Series
     returns: pd.Series
     '''
     return month.str.extract(r'\d\d\d\d(\d\d)')
+
+def make_date(year, month):
+    '''
+    year: pd.Series
+    month: pd.Series
+    returns: pd.Series
+    '''
+    return pd.to_datetime(year.astype(str) + '-' + month).dt.to_period('M').dt.to_timestamp('M')
