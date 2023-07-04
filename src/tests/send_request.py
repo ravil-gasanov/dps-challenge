44eee1b38bc080a2a5b64a790a3fc02e66d2f5d0
Ravil Gasanov <12788769+ravil-gasanov@users.noreply.github.com>
2023-07-04 13:39:05 +0400

    test web app with a request

---
+++ src/tests/send_request.py
@@ -0,0 +1,17 @@
+import requests
+
+def send_request():
+    url = "http://localhost:5000/predict"
+
+    for i in range(1, 13):
+        x_json = {
+            "year":2021,
+            "month":i
+        }
+
+        r = requests.post(url, json = x_json)
+
+        print(r.text.strip())
+
+if __name__ == "__main__":
+    send_request()
